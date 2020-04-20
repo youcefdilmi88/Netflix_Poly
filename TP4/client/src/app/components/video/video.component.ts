587aa93f7a1d6c87dbc1f9cacc4a0ae1c875e120
youcef_dilmi <youcefdilmi88@gmail.com>
2020-04-19 21:45:30 -0400

    last commit

--- TP4/client/src/app/components/video/video.component.ts
+++ TP4/client/src/app/components/video/video.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatVideoModule } from 'mat-video';
 import { Router} from '@angular/router';
+import { MemberService } from "../../services/member-service/memberService";
+import { Membre } from "../../models/Membre";
 
 @Component({
   selector: 'app-video',
@@ -9,13 +11,22 @@ import { Router} from '@angular/router';
 })
 export class VideoComponent implements OnInit {
 
-  constructor(public video: MatVideoModule, public router: Router) { }
+  constructor(public video: MatVideoModule, public router: Router, private memberService: MemberService) { }
+
+  activeMember: Membre | null;
 
   ngOnInit() {
+    this.memberService.obsMember.subscribe((obsMember: Membre | null) => this.activeMember = obsMember);
+
   }
 
   retour(){
-    this.router.navigateByUrl('/member');
+    if(this.activeMember  === null){}
+    else if (this.activeMember.isAdmin) {
+      this.router.navigateByUrl('/admin');
+    } else {
+        this.router.navigateByUrl('member');
+      }
   }
 
 }
